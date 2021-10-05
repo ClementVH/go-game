d6e53420986521874a5c714ea3d8554d39fa7c5f
ClementVH <clement.vanhecke@gmail.com>
2021-10-05 22:19:32 +0200

    move static shader code to static shader file

--- src/Shaders/static-shader.go
+++ src/Shaders/static-shader.go
@@ -54,3 +54,53 @@ func (shader *StaticShader) LoadLight(light *Entities.Light) {
 	loadVector(shader.lightPosition, light.Position)
 	loadVector(shader.lightColor, light.Color)
 }
+
+var VertexShader = `
+#version 330
+
+in vec3 position;
+in vec2 textureCoords;
+in vec3 normal;
+
+out vec2 pass_textureCoords;
+out vec3 surfaceNormal;
+out vec3 toLightVector;
+
+uniform mat4 transformationMatrix;
+uniform mat4 projectionMatrix;
+uniform mat4 viewMatrix;
+uniform vec3 lightPosition;
+
+void main(void) {
+	vec4 worldPosition = transformationMatrix * vec4(position, 1.0);
+	gl_Position = projectionMatrix * viewMatrix * worldPosition;
+	pass_textureCoords = textureCoords;
+
+	surfaceNormal = (transformationMatrix * vec4(normal, 0.0)).xyz;
+	toLightVector = lightPosition - worldPosition.xyz;
+}
+` + "\x00"
+
+var FragmentShader = `
+#version 330
+
+in vec2 pass_textureCoords;
+in vec3 surfaceNormal;
+in vec3 toLightVector;
+
+out vec4 out_Color;
+
+uniform sampler2D textureSampler;
+uniform vec3 lightColor;
+
+void main(void) {
+	vec3 unitNormal = normalize(surfaceNormal);
+	vec3 unitLightVector = normalize(toLightVector);
+
+	float nDot1 = dot(unitNormal, unitLightVector);
+	float brightness = max(nDot1, 0.4);
+	vec3 diffuse = brightness * lightColor;
+
+	out_Color = vec4(diffuse, 1.0) * texture(textureSampler, pass_textureCoords);
+}
+` + "\x00"
