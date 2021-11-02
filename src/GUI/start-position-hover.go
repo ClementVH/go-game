d9a1fe7919d60209ef37027c5ded797cc0e2a4c7
ClementVH <clement.vanhecke@gmail.com>
2021-11-03 00:43:32 +0100

    move player to clicked start position

--- src/GUI/start-position-hover.go
+++ src/GUI/start-position-hover.go
@@ -3,18 +3,27 @@ package GUI
 import (
 	"fmt"
 	"go-game/src/State"
+	"go-game/src/Systems"
 
 	"github.com/go-gl/glfw/v3.3/glfw"
+	"github.com/go-gl/mathgl/mgl32"
 )
 
 func (gui *GUI) runStartPositionHover(button glfw.MouseButton, action glfw.Action) {
 
 	if State.Combat.Combat != nil && button == glfw.MouseButton1 && action == glfw.Release {
 		var position, err = State.GUI.MousePicker.GetStartPosition()
+		chunk := State.Combat.Combat.GetChunk()
+		player := Systems.Player
 		if err == nil {
 			fmt.Println(position)
-		} else {
-			fmt.Println(err)
+			positionWorldCoordinates := mgl32.Vec3{
+				chunk.Position.X() + position.X(),
+				0,
+				chunk.Position.Z() + position.Y(),
+			}
+
+			player.MoveTo(positionWorldCoordinates)
 		}
 	}
 }
